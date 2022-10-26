bfaff43c137ed24a80009fd7d0a17fa3ca65d84a
wnstn819 <wnstn819@naver.com>
2022-10-26 17:47:02 +0900

    D2_1928 Begin

---
+++ src/D2/D2_1928/Solution.java
@@ -0,0 +1,24 @@
+package D2.D2_1928;/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int i=0;i<T;i++){
+           String str = sc.nextLine();
+            System.out.println("test" + str);
+
+
+
+
+        }
+
+    }
+}
