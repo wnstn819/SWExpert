f9ef33d9d75c45853624a774234d2bbb02bb00a6
wangjunsu <wnstn819@naver.com>
2022-10-29 18:57:51 +0900

    D2_1946 Start

---
+++ src/D2/D2_1946/Solution.java
@@ -0,0 +1,37 @@
+package D2.D2_1946;/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+
+
+        for(int i=0 ;i<T;i++){
+            String[] line = br.readLine().split(" ");
+            System.out.println("AAA"+ line[0]);
+            System.out.println("BBB"+ line[1]);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
