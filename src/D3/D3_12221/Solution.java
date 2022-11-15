dd386d5c25e6b8e62672bb5d73eefef45badd699
wnstn819 <wnstn819@naver.com>
2022-11-15 13:59:55 +0900

    D3_12221 구구단2

---
+++ src/D3/D3_12221/Solution.java
@@ -0,0 +1,30 @@
+package D3.D3_12221;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        for(int testcase=0;testcase<n;testcase++){
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            if(m >9 || k>9){
+                sb.append("#" + (testcase+1) + " " +-1+"\n");
+            }else{
+                sb.append("#" + (testcase+1) + " " +(m*k) +"\n");
+            }
+
+
+        }
+        System.out.println(sb);
+
+    }
+}
